a4672401172c824f78247549f43ecd7d3e28366d
kiranbukka7 <119900279+kiranbukka7@users.noreply.github.com>
2023-05-16 20:20:30 -0700

    Added logging

--- src/main/java/com/personal/springbootdemo/controllers/TestRestController.java
+++ src/main/java/com/personal/springbootdemo/controllers/TestRestController.java
@@ -1,12 +1,27 @@
 package com.personal.springbootdemo.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
+/**
+ * Test REST API Methods.
+ */
 @RestController
 public class TestRestController {
+
+    private static final Logger logger = LoggerFactory.getLogger(TestRestController.class);
+    /**
+     * Get Method to return welcome message.
+     *
+     * @return welcome message.
+     */
     @GetMapping(value = "/welcome")
     public static String welcome() {
+        logger.info("Test Info level");
         return "welcome to springboot";
     }
 }
